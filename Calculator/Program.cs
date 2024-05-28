1ff54f3bda8674f475944b9c3c4402cbe4b5ab88
Keman <keatvun191@gmail.com>
2024-05-28 15:22:12 +0800

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            string function;
+            bool restart;
+
+            Console.WriteLine("Welcome To Calculator Program!");
+
+            do
+            {
+                Console.WriteLine("Please Enter Your First Number: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please Enter Your Second Number: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Your Desired Function(Symbol): ");
+                Console.WriteLine("Add --- + ");
+                Console.WriteLine("Substract --- - ");
+                Console.WriteLine("Multiply --- * ");
+                Console.WriteLine("Divide --- / ");
+                function = Console.ReadLine();
+
+                switch (function) {
+                    case "+":
+                        Console.WriteLine(num1 + num2);
+                        break;
+                    case "-":
+                        Console.WriteLine(num1 - num2);
+                        break;
+                    case "*":
+                        Console.WriteLine(num1 * num2);
+                        break;
+                    case "/":
+                        Console.WriteLine(num1 / num2);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Function!");
+                        break;
+                }
+                Console.WriteLine("Do you want to restart the calculator? (Y/N)");
+                if (Console.ReadLine() == "Y")
+                {
+                    restart = true;
+                }
+                else
+                {
+                    restart = false;
+                }
+            } while (restart == true);
+        }
+    }
+}
